91ba064c9d491fc1e95148f045fcc375c138381a
kento.yamanaka <kento.tora@gmail.com>
2018-09-10 10:02:16 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,6 +5,9 @@
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
     
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    
+        {!! Form::label('title', 'タイトル:') !!}
+        {!! Form::text('title') !!}
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
