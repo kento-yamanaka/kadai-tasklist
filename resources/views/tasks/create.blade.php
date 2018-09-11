da088b8a0b48897a01b161cf12800345cceae854
kento.yamanaka <kento.tora@gmail.com>
2018-09-11 10:04:59 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,19 +4,43 @@
 
     <h1>タスク新規作成ページ</h1>
     
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    <!--{!! Form::model($task, ['route' => 'tasks.store']) !!}-->
     
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+    <!--    {!! Form::label('status', 'ステータス:') !!}-->
+    <!--    {!! Form::text('status') !!}-->
     
-        {!! Form::label('title', 'タイトル:') !!}
-        {!! Form::text('title') !!}
+    <!--    {!! Form::label('title', 'タイトル:') !!}-->
+    <!--    {!! Form::text('title') !!}-->
 
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+    <!--    {!! Form::label('content', 'タスク:') !!}-->
+    <!--    {!! Form::text('content') !!}-->
 
-        {!! Form::submit('投稿') !!}
+    <!--    {!! Form::submit('投稿') !!}-->
 
-    {!! Form::close() !!}
+    <!--{!! Form::close() !!}-->
+    <div class="row">
+        <div class="col-xs-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                <div class="form-group">
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>    
     
 @endsection
