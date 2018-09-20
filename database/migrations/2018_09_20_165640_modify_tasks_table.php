a49fe2be7a7b7548e5b6b3e0c168a88a227d865a
kento.yamanaka <kento.tora@gmail.com>
2018-09-20 12:04:54 +0000

    Procfile

--- database/migrations/2018_09_20_165640_modify_tasks_table.php
+++ database/migrations/2018_09_20_165640_modify_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class ModifyTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,8 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->index();
             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users');
         });
@@ -26,6 +27,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('user_id');
     }
 }
